524aa03bfa8d4b4f8de53e14658b450c15994fd6
Quartz7878 <mmaahhddiieebbeennee@gmail.com>
2025-05-16 11:22:22 +0600

    Create constants.js

---
+++ modules/constants.js
@@ -0,0 +1,14 @@
+export const GRAVITY = 0.5;
+export const JUMP_FORCE = -12;
+export const MOVE_SPEED = 5;
+
+export const COLORS = {
+    player: '#0ff',
+    ground: '#338',
+    platform: '#559',
+    enemy: '#f55',
+    coin: '#ff0',
+    oxygen: '#5df',
+    bullet: '#ff5',
+    jetpack: '#f80'
+};
